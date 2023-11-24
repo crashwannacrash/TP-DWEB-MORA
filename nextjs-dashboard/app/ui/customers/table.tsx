91f164570c3d76d95d04639d66fcd312565a5966
sl <sacha.lefetz@gmail.com>
2023-11-24 14:32:58 +0100

    dsqsdq

--- nextjs-dashboard/app/ui/customers/table.tsx
+++ nextjs-dashboard/app/ui/customers/table.tsx
@@ -29,9 +29,9 @@ export default async function CustomersTable({
                         <div className="mb-2 flex items-center">
                           <div className="flex items-center gap-3">
                             <Image
+                              alt='profile picture'
                               src={customer.image_url}
                               className="rounded-full"
-                              alt='profile picture'
                               width={28}
                               height={28}
                             />
@@ -86,9 +86,9 @@ export default async function CustomersTable({
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
                           <Image
+                            alt='profile picture'
                             src={customer.image_url}
                             className="rounded-full"
-                            alt='profile picture'
                             width={28}
                             height={28}
                           />
